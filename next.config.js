af3234c092cf847ed85306aadc4b7c078a4a0aeb
no0r06 <noorzohbi8@gmail.com>
2026-02-03 18:12:06 +0200

    Fix formatting in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // CRITICAL FOR NETLIFY
+  output: 'export',
   reactStrictMode: true,
-  images: { unoptimized: true },  // CRITICAL
+  images: { unoptimized: true }
 }
 
 module.exports = nextConfig
