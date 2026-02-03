87868fbf93539f19556b04004252e5face9e926d
no0r06 <noorzohbi8@gmail.com>
2026-02-03 18:00:10 +0200

    Configure output for Netlify in next.config.js
    Added output configuration for Netlify deployment.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // CRITICAL FOR NETLIFY
   reactStrictMode: true,
-   images: { unoptimized: true },
+  images: { unoptimized: true },  // CRITICAL
 }
 
 module.exports = nextConfig
